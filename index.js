9b87c487bc80f967866458cdb735c84c9f08412b
Jerrod <jlankford@salesforce.com>
2020-03-15 15:42:26 -0400

    Stuff

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/:name', (req, res) => {
 });
 
 // ssl route
-app.get('/.well-known/acme-challenge/NEjy81_5PqXyjnyNhPzOJZhGua9EJqsZYuwlJdLCZ7k', (req, res) => {
-  res.send('NEjy81_5PqXyjnyNhPzOJZhGua9EJqsZYuwlJdLCZ7k.r55yryYu5PWXFU75NIVYt0_KaNoKHShqsBjD3LqX8aM');
+app.get('/.well-known/acme-challenge/3XUzlpNUapJIOqavEg7778bdjvATRiXSvNhxNiY_3gg', (req, res) => {
+  res.send('3XUzlpNUapJIOqavEg7778bdjvATRiXSvNhxNiY_3gg.r55yryYu5PWXFU75NIVYt0_KaNoKHShqsBjD3LqX8aM');
 });
 
 wss.on('connection', function connection(ws) {
